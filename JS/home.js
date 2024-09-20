5a1e391e539b001c9212c2139823e185337c473a
Rafiul-Ovi <rafiulovi11@gmail.com>
2024-09-20 22:54:56 +0600

    added third commit

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,19 @@
-console.log("home added");
+// add money to the account
+/*
+s-1: add event handler(prevent page reload after from submit)
+s-2: get money to be added to the account balance
+*/
+
+// step-1: add an event handler to the add money button inside the form
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // step-2: get money to be added to the account balance
+    const addMoneyInput = document.getElementById("input-add-money").value;
+    // get the pin number provided
+    const pinNumber = document.getElementById("input-pin-number").value;
+
+    console.log(addMoneyInput, pinNumber);
+  });
